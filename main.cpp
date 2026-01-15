627b566eebbb2e1ffe1e322f322fb542bc7b188c
CreepySenpai <tuana8255@gmail.com>
2026-01-15 23:54:20 +0700

    Adjust main

--- main.cpp
+++ main.cpp
@@ -1,30 +1,60 @@
 #include <Creepy/Logger.hpp>
 #include <Creepy/Assert.hpp>
 #include <Creepy/DynArray.hpp>
+#include <Creepy/Node.hpp>
 
 int main(){
 
-    uint8_t* buffer = new uint8_t[30 * 4096]{};
+    constexpr uint64_t ARENA_SIZE = 30 * 4096;
+    uint8_t* buffer = new uint8_t[ARENA_SIZE]{};
 
     Creepy::Arena arena{
         .mem = buffer,
         .count = 0,
-        .capacity = 4096
+        .capacity = ARENA_SIZE
     };
 
     Creepy::Arena_Align(arena, 8);
 
-    auto myArr = Creepy::DynArray_Create<int>(arena, 20);
+    // auto myArr = Creepy::DynArray_Create<int>(arena, 20);
 
-    for(uint32_t i{}; i < myArr.capacity; ++i){
-        myArr.element[i] = i;
+    // for(uint32_t i{}; i < myArr.capacity; ++i){
+    //     myArr.element[i] = i;
+    // }
+
+    // for(uint32_t i{}; i < myArr.capacity; ++i){
+    //     ASSERT_MSG(myArr.element[i] != i, "Hum wrong");
+
+    //     LOG("Ele: {}", myArr.element[i]);
+    // }
+
+    Creepy::NodeContainer nodeContainer = Creepy::NodeContainer_CreateNodeContainer(arena, 100);
+
+    std::println("Done A");
+
+    for(uint32_t i{}; i < 10; ++i){
+        Creepy::Node_CreateStartNode(nodeContainer);
     }
 
-    for(uint32_t i{}; i < myArr.capacity; ++i){
-        ASSERT_MSG(myArr.element[i] != i, "Hum wrong");
+    std::println("Done B");
 
-        LOG("Ele: {}", myArr.element[i]);
+    for(uint32_t i{}; i < 10; ++i){
+        Creepy::Node_CreateConstantNode(nodeContainer, Creepy::NodeValueType::Uint32, 69 + i);
     }
+
+    std::println("Done C");
+
+    for(uint32_t i{}; i < 10; ++i){
+        Creepy::Node_CreateReturnNode(nodeContainer, i, i + 10);
+    }
+
+    std::println("Done D");
+
+    for(uint32_t i{}; i < nodeContainer.nodes.count; ++i){
+        Creepy::Node_PrintNodeInfo(nodeContainer, i);
+    }
+
+    std::println("Done E");
 
     return 0;
 }
