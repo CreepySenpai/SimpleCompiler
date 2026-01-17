3ce25aae9a1d23631cb1d46ed13b89996f83d334
CreepySenpai <tuana8255@gmail.com>
2026-01-18 00:09:27 +0700

    Align mem after alloc mem

--- include/Creepy/Arena.hpp
+++ include/Creepy/Arena.hpp
@@ -11,14 +11,30 @@ namespace Creepy {
         uint64_t capacity{};
     };
 
+    constexpr inline void Arena_Align(Arena& arena, uint64_t alignSize){
+        const uint64_t remain = alignSize - (reinterpret_cast<uint64_t>(arena.mem) % alignSize);
+
+        if(remain == 0){
+            return;
+        }
+
+        ASSERT_MSG((arena.count + remain) < arena.capacity, "Hum bro align make size bigger than capacity");
+
+        arena.count += remain;
+    }
+
     template <typename T>
     constexpr inline T* Arena_Alloc(Arena& arena){
         if(arena.count + sizeof(T) >= arena.capacity){
+            LOG("Too big bro: {} - {}", arena.capacity, (arena.count + sizeof(T)));
             return nullptr;
         }
         
         T* obj = std::start_lifetime_as<T>(static_cast<uint8_t*>(arena.mem) + arena.count);
+        
         arena.count += sizeof(T);
+
+        Arena_Align(arena, 8);
 
         return obj;
     }
@@ -32,22 +48,14 @@ namespace Creepy {
             return nullptr;
         }
 
-        T*obj = std::start_lifetime_as_array<T>(static_cast<uint8_t*>(arena.mem) + arena.count, count);
-
+        T* obj = std::start_lifetime_as_array<T>(static_cast<uint8_t*>(arena.mem) + arena.count, count);
+        
         arena.count += sizeToAlloc;
+
+        Arena_Align(arena, 8);
 
         return obj;
     }
 
-    constexpr inline void Arena_Align(Arena& arena, uint64_t alignSize){
-        const uint64_t remain = alignSize - (reinterpret_cast<uint64_t>(arena.mem) % alignSize);
-
-        if(remain == 0){
-            return;
-        }
-
-        ASSERT_MSG((arena.count + remain) < arena.capacity, "Hum bro align make size bigger than capacity");
-
-        arena.count += remain;
-    }
+    
 }
